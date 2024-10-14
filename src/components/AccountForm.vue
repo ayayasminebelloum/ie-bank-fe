bb6f2e9e53448a4816a3b19bb159bb903566c511
ayayasmine <yasminayabelloum1s5@gmail.com>
2024-10-14 22:54:20 +0200

    Added home page and country field to account form

---
+++ src/components/AccountForm.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+      <form @submit="submitForm">
+        <!-- Other form fields -->
+        <label for="country">Country:</label>
+        <input type="text" v-model="account.country" id="country" required />
+        <button type="submit">Create Account</button>
+      </form>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        account: {
+          country: '',
+          // other fields
+        }
+      };
+    },
+    methods: {
+      submitForm() {
+        // Handle form submission
+      }
+    }
+  }
+  </script>
+  
---
+++ src/components/home.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+      <h1>Welcome to IE Bank</h1>
+      <button @click="goToAccounts">Go to Accounts</button>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    methods: {
+      goToAccounts() {
+        this.$router.push('/accounts');
+      }
+    }
+  }
+  </script>
+  
